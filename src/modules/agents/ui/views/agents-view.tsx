1df9638f2f34a9c48aa806f00184d6cae5167a37
coder-Yash886 <yashkumar.967565@gmail.com>
2026-06-25 09:17:15 +0530

    Adding a loading state

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,12 +1,20 @@
 "use client"
 import { useQuery } from "@tanstack/react-query"
 import { trpc } from "@/trpc/client"
+import { LoadingState } from "@/components/loading-state";
 
 export const AgentsView = () => {
   const { data, isLoading, isError } = trpc.agents.getMany.useQuery();
 
-  if (isLoading)
-     return <div>Loading...</div>
+  if (isLoading){
+    return (
+        <LoadingState
+        title= "Loading Agents"
+        description= "This will take few seconds...."
+        />
+    )
+  }
+   
   if (isError)
      return <div>Error</div>
 
